bde97ed9f90e755139322cf5b83f09e6dfd08046
4html <jfisher@4html.com>
2020-10-14 12:18:10 -0700

    Adjusting metric layout to no longer use flex.

--- projects/ng-mix-ui/src/lib/metric/metric.component.ts
+++ projects/ng-mix-ui/src/lib/metric/metric.component.ts
@@ -9,29 +9,24 @@ import { AfterViewInit, Component, ElementRef, Input, OnInit, Renderer2, ViewEnc
 export class MetricComponent implements OnInit, AfterViewInit {
 
     @Input() caption: string;
-    @Input('caption-side') captionSide: string;
     @Input('style-caption') captionStyle: {};
     @Input() value: string;
     @Input('style-value') valueStyle: {};
 
     constructor(private element: ElementRef, private renderer: Renderer2) { }
 
-    ngOnInit(): void {
-        this.captionSide = this.captionSide === 'top' ? 'top' : 'bottom';
-    }
+    ngOnInit(): void { }
 
     ngAfterViewInit(): void {
         const element = this.element.nativeElement;
         if (this.valueStyle) {
-            const index = this.caption && this.captionSide === 'top' ? 1 : 0;
             Object.keys(this.valueStyle).forEach(key => {
-                this.renderer.setStyle(element.children[index], key, this.valueStyle[key]);
+                this.renderer.setStyle(element.children[1], key, this.valueStyle[key]);
             });
         }
         if (this.caption && this.captionStyle) {
-            const index = this.captionSide === 'top' ? 0 : 1;
             Object.keys(this.captionStyle).forEach(key => {
-                this.renderer.setStyle(element.children[index], key, this.captionStyle[key]);
+                this.renderer.setStyle(element.children[1], key, this.captionStyle[key]);
             });
         }
     }
